f646e29d773a49668581c311e35fc612f980fed7
RTKartograph <tommasokarolrendo@gmail.com>
2025-10-23 15:54:36 +0200

    Fixes

--- [Bash]ARP_Scanner/arpscanner.sh
+++ [Bash]ARP_Scanner/arpscanner.sh
@@ -5,6 +5,23 @@ iface=""
 network_cidr=""
 timeout=1
 pp=50
+out_csv=""
+
+usage() {
+	cat <<EOF
+Usage: $0 -i=interface -n=network[/cidr] [-t=timeout] [-p=parallel] [-o=out.csv]
+
+Example: sudo $0 -i=eth0 -n=192.168.1.0/24 -t=1 -p=50 -o=list.csv
+
+Options:
+	-i iface	Network interface to use (Required)
+	-n network	Network in CIDR Notation (Required), e.g. 192.168.1.0/24
+	-t timeout	Arping timeout in seconds (default: 1)
+	-p parallel	How many requests done in parallel (default: 50)
+	-o output	Generates CSV output file
+EOF
+	exit 1
+}
 
 ip2int() {
 	local a b c d ip=$1 
@@ -39,27 +56,38 @@ for i in "$@"; do
 	case $i in
 		-i=* | --interface=*)
 			iface="${i#*=}"
-			shift
 			;;
 		-n=* | --network=*)
 			network_cidr="${i#*=}"
-			shift
 			;;
 		-t=* | --timeout=*)
 			timeout="${i#*=}"
-			shift
 			;;
 		-p=* | --parallel=*)
 			pp="${i#*=}"
-			shift
+			;;
+		-o=* | --output=*)
+			out_csv="${i#*=}"
+			;;
+		-h | --help )
+			usage
+			;;
+		*) 
+			echo "Unknown option: $i" >&2;
+			usage
 			;;
 	esac
 done
+
+if [[ -z "$iface" || -z "$network_cidr" ]]; then
+	usage
+fi
 
 if [[ "$network_cidr" != */* ]]; then
 	echo "Network must be in CIDR form! e.g. 192.168.0.1/24" >&2
 	exit 1
 fi
+
 network="${network_cidr%/*}"
 cidr="${network_cidr##*/}"
 
@@ -72,8 +100,6 @@ broadcast=$(( network_base | ((~cidr2mask) & 0xFFFFFFFF) ))
 start_ip=$(( network_base + 1 ))
 end_ip=$(( broadcast - 1 ))
 
-printf "Start IP:%s\nEnd_IP:%s\n" $start_ip, "$end_ip"
-
 if (( start_ip > end_ip )); then
 	echo "No hosts to scan." >&2
 	exit 1
@@ -85,12 +111,15 @@ scan_job() {
 	local local_timeout=$2
 	local local_iface=$3
 	
-	arping -c 1 -w "$local_timeout" -I "$iface_local" "$ip" >/dev/null 2>&1
+	arping -c 1 -w "$local_timeout" -I "$local_iface" "$ip" >/dev/null 2>&1
 	if [[ $? -eq 0 ]]; then
 		# ARP reply received
-		mac=$(ip neigh show "$ip" 2>/dev/null)
+		mac=$(ip neigh show "$ip" 2>/dev/null | awk '/lladdr/ {print $5; exit}')
 		mac=${mac:-unknown}
-		printf '%s\t$s\n' "$ip" "$mac"
+		printf '%s\t%s\n' "$ip" "$mac"
+		if [[ -n "$out_csv" ]]; then
+			printf "%s,%s,%s\n" "$ip" "$mac" "$iface_local" >> "$out_csv"
+		fi
 	fi
 }
 
@@ -98,13 +127,13 @@ export -f scan_job
 
 echo "Scanning $network_cidr on interface $iface..."
 echo "Results:"
-printf "%-16s %n" "IP" "MAC"
+printf "%-16s %s\n" "IP" "MAC"
 for (( ip=start_ip; ip<=end_ip; ip++ )); do
 	ip_str=$(int2ip "$ip")
 	scan_job "$ip_str" "$timeout" "$iface" &
 	((job_count++))
 
-	if (( job_count >= parallel )); then
+	if (( job_count >= pp )); then
 		wait -n
 		job_count=$(jobs -p | wc -l)
 	fi
@@ -113,3 +142,6 @@ done
 wait
 
 echo "Scan finished."
+if [[ -n $out_csv ]]; then
+	echo "Sucessfully saved in: $out_csv"
+fi
